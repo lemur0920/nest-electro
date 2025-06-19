e3f18627db7dd4d95f5b971bb4223af49d967855
lemur0920 <lemur12@naver.com>
2025-06-19 22:40:40 +0900

    initial commit

---
+++ backend/src/app.module.ts
@@ -0,0 +1,23 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config'
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+import { ProductModule } from './product/product.module';
+import { CartModule } from './cart/cart.module';
+import { OrderModule } from './order/order.module';
+import { ReviewModule } from './review/review.module';
+import { PrismaModule } from 'prisma/prisma.module';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: '.env',
+      isGlobal: true
+    }),
+    PrismaModule, UserModule, ConfigModule, ProductModule, CartModule, OrderModule, ReviewModule
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
