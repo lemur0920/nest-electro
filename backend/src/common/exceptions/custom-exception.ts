8aa963fe61095a9653ad4eb229b4e70a28cec31a
lemur0920 <lemur12@naver.com>
2025-06-21 19:49:41 +0900

    포맷 수정

--- backend/src/common/exceptions/custom-exception.ts
+++ backend/src/common/exceptions/custom-exception.ts
@@ -2,19 +2,25 @@ import { HttpException } from "@nestjs/common";
 
 export const EXCEPTION_STATUS = {
   // AUTH
-  LOGIN_TOKEN_ERROR: {
-    errorCode: 10001,
-    message: '올바른 토큰이 아닙니다.',
-    statusCode: 401,
+  AUTH: {
+    INVALID_TOKEN: {
+      errorCode: 10001,
+      message: '유효하지 않은 토큰입니다..',
+      statusCode: 401,  
+    },
+    TOKEN_EXPIRED: {
+      errorCode: 10002,
+      message: '토큰이 만료되었습니다.',
+      statusCode: 401,
+    },
   },
-  USER_NOT_FOUND: {
-    errorCode: 10002,
-    message: '해당 유저가 존재하지 않습니다.',
-    statusCode: 404,
+  USER: {
+    NOT_FOUND: {
+      errorCode: 10002,
+      message: '해당 유저가 존재하지 않습니다.',
+      statusCode: 404,
+    },
   },
-  
-
-  
 }
 
 export class CustomException extends HttpException {
